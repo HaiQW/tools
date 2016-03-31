c43aac59cde707e914645090271b0f5b93afd476
HaiQW <hq_weng@zju.edu.cn>
2016-03-31 15:44:09 +0800

    添加文件

--- figure/visulization.py
+++ figure/visulization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 # -*- coding:utf-8 -*-
 """visuliza and explore the synthetic data set"""
+import os
 import random
 import numpy as np
 import pandas as pd
@@ -25,23 +26,32 @@ def get_data(data_set, label_set, size_set):
     return target
 
 
-def scatter_2d(x, y):
-    """2d散点图,画label propatation算法的可视化效果. x数据样本,y是数据样本的标签(label)."""
+def scatter_2d(data_set):
+    """2d散点图,画label propatation算法的可视化效果. data_set 是包含了标签的数据样本集合.
+    红色三角形表示rare category,蓝色圆形表示major category."""
     fig = plt.figure()
-    plt.scatter(x[:, 0], x[:, 1], c=y, edgecolor='')
-    plt.show(fig)
-    return
+    labels = np.unique(data_set[:, -1])
+    plt.scatter(data_set[data_set[:, -1] == labels[0], 0], data_set[data_set[:, -1] == labels[0], 1], c='red', s=30, marker='^', edgecolor='')
+    plt.scatter(data_set[data_set[:, -1] == labels[1], 0], data_set[data_set[:, -1] == labels[1], 1], c='blue', s=15, marker='o', edgecolor='')
+    return fig
 
 
 def main():
     """Main 函数"""
-    df = pd.read_csv('synthetic_data.txt', delimiter=' ')
-    print df.describe
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'synthetic_data_1.txt')
 
-    dataset = get_data(df.values, [2, 5], [50, 1000])
-    print dataset.shape
-    # fig = plt.figure()
-    scatter_2d(dataset[:, 0:2], dataset[:, 3])
+    # 选取部分数据画图，并保存
+    if not os.path.isfile(file_path):
+        origin_data = np.loadtxt('synthetic_data.txt')
+        data_set = get_data(origin_data, [2, 5], [30, 500])
+        print file_path
+        np.savetxt(file_path, data_set)
+    else:
+        data_set = np.loadtxt(file_path)
+
+    # 散点图1
+    fig_scatter_2d = scatter_2d(data_set[:, [0, 1, 3]])
+    plt.show()
 
 
 if __name__ == '__main__':
