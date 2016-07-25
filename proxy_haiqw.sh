fd981fa6c2866715e25316a0e60510f877cff2c6
root <root@Lab.309>
2016-07-25 10:48:50 -0400

    update proxy file

--- proxy_haiqw.sh
+++ proxy_haiqw.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#use  . proxy_wb.sh
-http_proxy="http://root:root@10.214.16.213:808"
-export http_proxy="http://root:root@10.214.16.213:808"
-#export http_proxy="http://10.214.16.212:8888"
-https_proxy="http://root:root@10.214.16.213:808"
-export https_proxy="http://root:root@10.214.16.213:808"
+#use  haiqw desktop proxy
+export http_proxy="http://10.214.16.100:8888"
+export https_proxy="http://10.214.16.100:8888"
+
+# export http_proxy="http://root:root@10.214.16.213:808"
+# export https_proxy="http://root:root@10.214.16.213:808"
+
 #apt-get update
-#export GIT_PROXY_COMMAND="/share/yc/git-proxy.sh"
-export GIT_PROXY_COMMAND="/share/yc/git-proxy.sh"
-#http_proxy="http://wb:wb@10.214.16.21:808"
-#https_proxy="http://wb:wb@10.214.16.21:808"
+
+# export GIT_PROXY_COMMAND="/share/yc/git-proxy.sh"
+# export GIT_PROXY_COMMAND="/share/yc/git-proxy.sh"
